689375797016b117c8bc956ba338f079d64f877b
martin_grman <martingrman@outlook.com>
2019-01-04 19:52:57 +0100

    simple: fixed simple sample

--- Assets/Votyra/Core/Images/Image2iTo2fProvider.cs
+++ Assets/Votyra/Core/Images/Image2iTo2fProvider.cs
@@ -9,7 +9,7 @@ namespace Votyra.Core.Images
     public class Image2iTo2fProvider : IImage2fProvider
     {
         private readonly IImage2iProvider _provider;
-        
+
         public Image2iTo2fProvider(IImage2iProvider provider)
         {
             _provider = provider;
@@ -18,52 +18,55 @@ namespace Votyra.Core.Images
 
         public IImage2f CreateImage()
         {
-            return null;
-//            var image = _provider.CreateImage();
-//            if (image is IImageInvalidatableImage2i)
-//            {
-//                return  new Image2fInvalidatableWrapper(image);
-//            }
-//            else
-//            {
-//                return new Image2fWrapper(image);
-//            }
+            var image = _provider.CreateImage();
+            if (image is IImageInvalidatableImage2i)
+            {
+                return new Image2fInvalidatableWrapper(image);
+            }
+            else
+            {
+                return new Image2fWrapper(image);
+            }
         }
 
-//        private class Image2fWrapper : IImage2f
-//        {
-//            public Image2fWrapper(IImage2i image)
-//            {
-//                _image = image;
-//            }
-//
-//            private readonly IImage2i _image;
-//
-//            public Range1hf RangeZ => _image.RangeZ.ToRange1hf();
-//
-//            public Height1f Sample(Vector2i point)
-//            {
-//                return _image.Sample(point).ToHeight1f();
-//            }
-//        }
-//
-//        private class Image2fInvalidatableWrapper : IImage2f,IImageInvalidatableImage2i
-//        {
-//            public Image2fInvalidatableWrapper(IImage2i image)
-//            {
-//                _image = image;
-//            }
-//
-//            private readonly IImage2i _image;
-//
-//            public Range1hf RangeZ => _image.RangeZ.ToRange1hf();
-//
-//            public Height1f Sample(Vector2i point)
-//            {
-//                return _image.Sample(point).ToHeight1f();
-//            }
-//
-//            public Range2i InvalidatedArea => (_image as IImageInvalidatableImage2i).InvalidatedArea;
-//        }
+        private class Image2fWrapper : IImage2f
+        {
+            public Image2fWrapper(IImage2i image)
+            {
+                _image = image;
+            }
+
+            protected readonly IImage2i _image;
+
+            public Range1hf RangeZ => _image.RangeZ.ToRange1hf();
+
+            public Height1f Sample(Vector2i point)
+            {
+                return _image.Sample(point).ToHeight1f();
+            }
+
+            public IPoolableMatrix2<Height1f> SampleArea(Range2i area)
+            {
+                var min = area.Min;
+
+                var matrix = PoolableMatrix<Height1f>.CreateDirty(area.Size);
+                matrix.Size.ForeachPointExlusive(matPoint =>
+                {
+                    matrix[matPoint] = Sample(matPoint+min);
+                });
+                return matrix;
+            }
+        }
+
+        private class Image2fInvalidatableWrapper : Image2fWrapper, IImageInvalidatableImage2i
+        {
+            public Image2fInvalidatableWrapper(IImage2i image)
+                :base(image)
+            {
+            }
+
+
+            public Range2i InvalidatedArea => (_image as IImageInvalidatableImage2i).InvalidatedArea;
+        }
     }
 }
