10880f50646b80448019272ffd353a7da3fa52ec
martin_grman <martingrman@outlook.com>
2020-05-25 23:12:18 +0200

    better debug messages

--- Assets/Votyra/Plannar.Unity/MeshCreation/TerrainUnityMeshManager.cs
+++ Assets/Votyra/Plannar.Unity/MeshCreation/TerrainUnityMeshManager.cs
@@ -17,11 +17,14 @@ namespace Votyra.Plannar.Unity
 
         private IUnityTerrainGeneratorManager2i _manager;
         private ITerrainGameObjectPool _gameObjectPool;
+        private readonly ILayerConfig _layerConfig;
 
-        public TerrainUnityMeshManager(IUnityTerrainGeneratorManager2i manager, ITerrainGameObjectPool gameObjectPool, [Inject(Id = "root")] GameObject root)
+        public TerrainUnityMeshManager(IUnityTerrainGeneratorManager2i manager, ITerrainGameObjectPool gameObjectPool, [Inject(Id = "root")]
+            GameObject root, ILayerConfig layerConfig)
         {
             _manager = manager;
             _gameObjectPool = gameObjectPool;
+            this._layerConfig = layerConfig;
             _manager.NewTerrain += NewTerrain;
             _manager.ChangedTerrain += ChangedTerrain;
             _manager.RemovedTerrain += RemovedTerrain;
@@ -29,12 +32,14 @@ namespace Votyra.Plannar.Unity
 
         private void ChangedTerrain(Vector2i group, ITerrainMesh2f mesh)
         {
+#if UNITY_EDITOR
             if (!_unityMeshes.ContainsKey(group))
             {
                 Debug.LogError($"Changed {group} before NewTerrain was called!");
                 return;
             }
-            Debug.Log("Changed "+group);
+#endif
+            Debug.Log("Changed " + group);
             var pooledGameObject = _unityMeshes[group];
 
             mesh.SetUnityMesh(pooledGameObject);
@@ -42,6 +47,13 @@ namespace Votyra.Plannar.Unity
 
         private void RemovedTerrain(Vector2i group, ITerrainMesh2f mesh)
         {
+#if UNITY_EDITOR
+            if (!_unityMeshes.ContainsKey(group))
+            {
+                Debug.LogError($"RemovedTerrain {group} called without NewTerrain first!");
+                return;
+            }
+#endif
             Debug.Log("Removed " + group);
             var pooledGameObject = _unityMeshes[group];
             _gameObjectPool.ReturnRaw(pooledGameObject);
@@ -51,10 +63,20 @@ namespace Votyra.Plannar.Unity
 
         private void NewTerrain(Vector2i group, ITerrainMesh2f mesh)
         {
+#if UNITY_EDITOR
+            if (_unityMeshes.ContainsKey(group))
+            {
+                Debug.LogError($"NewTerrain {group} called multiple times without RemovedTerrain after each time!");
+                return;
+            }
+#endif
+
             Debug.Log("New     " + group);
             var pooledGameObject = _gameObjectPool.GetRaw();
             if (!pooledGameObject.IsInitialized)
                 pooledGameObject.Initialize();
+
+            pooledGameObject.GameObject.name = $"Group_{group}_{_layerConfig.Layer}_{this.GetHashCode()}";
             pooledGameObject.SetActive(true);
             mesh.SetUnityMesh(pooledGameObject);
 
