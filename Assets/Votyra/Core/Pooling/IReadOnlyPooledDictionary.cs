dc9353e367e527adb1820d197d2604793cb0c4ca
Martin Grman <martingrman@outlook.com>
2021-09-12 20:04:42 +0200

    removed more dead code

--- Assets/Votyra/Core/Pooling/IReadOnlyPooledDictionary.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-namespace Votyra.Core.Pooling
-{
-    public interface IReadOnlyPooledDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>, IReadOnlyPooledCollection<KeyValuePair<TKey, TValue>>, IDisposable
-    {
-    }
-}
